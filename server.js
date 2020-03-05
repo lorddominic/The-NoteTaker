609fb35e4ef895894c3f1d1c88818b2b642446c8
Boyan Xu <dominictxu1@gmail.com>
2020-03-05 10:12:57 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+})
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
