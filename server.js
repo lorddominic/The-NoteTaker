1f7bcbe1e357af8c517fb92a680d32728bf92863
Boyan Xu <dominictxu1@gmail.com>
2020-03-05 10:59:43 -0700

    updated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require("express");
+const express = require("express");
 var path = require("path");
 
-var app = express();
-var PORT = 3000;
+const app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +11,11 @@ app.use(express.json());
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 })
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"))
-})
+app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "public/index.html"))
+    })
+    // app.get("*", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "public/index.html"))
+    // })
+
+app.listen(PORT, () => console.log("nice"));
